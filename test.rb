a88ce25ad5668a67731f22873037c9b1c9720001
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-01-12 03:18:41 -0600

    SlackAPI-Auth-ok
    Authorization test ok, added api token

--- test.rb
+++ test.rb
@@ -1,5 +1,7 @@
 require 'http'
 require 'json'
 
-rc = HTTP.post("https://slack.com/api/api.test")
+rc = HTTP.post("https://slack.com/api/auth.test", params: {
+  token: ENV['SLACK_API_TOKEN']
+})
 puts JSON.pretty_generate(JSON.parse(rc.body))
