436182b4247708ef7cd7ff0921d1e98c59a6b033
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-01-13 10:15:25 -0600

    bot deploytorocketbots

--- test.rb
+++ test.rb
@@ -1,10 +1,11 @@
 require 'http'
 require 'json'
 
-rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
-  token: ENV['SLACK_API_TOKEN'],
-  channel: '#general',
-  text: 'HelloWorld',
-  as_user: true
-})
-puts JSON.pretty_generate(JSON.parse(rc.body))
+get '/'do
+  if params['code']
+    rc = HTTP.post("https://slack.com/api/oauth.access", params: {
+      code: params['code'],
+      client_id: ENV['SLACK_CLIENT_ID'],
+      client_secret: ENV['SLACK_CLIENT_SECRET']
+      })
+end
