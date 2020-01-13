660ea05fa589422f5f3618126c228306e2e5b521
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-01-13 01:57:29 -0600

    Giphy_response_added

--- slack-imojionbot/commands/call.rb
+++ slack-imojionbot/commands/call.rb
@@ -2,7 +2,7 @@ module SlackImojionbot
   module Commands
     class Call < SlackRubyBot::Commands::Base
       command 'call'  do |client, data, _match|
-        client.say(channel: data.channel, text: 'called')
+        client.say(channel: data.channel, text: 'called', gif: 'kitten')
       end
     end
   end
