3f5dd46470aab34575f0c882282e86434aa4e986
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-01-14 22:22:07 -0600

    rpec failing test on reddit command fixed

--- slack-imojionbot/commands/reddit.rb
+++ slack-imojionbot/commands/reddit.rb
@@ -5,7 +5,7 @@ module SlackImojionbot
   module Commands
     class Reddit < SlackRubyBot::Commands::Base
         command 'reddit' do |client, data, _match|
-          url = 'https://reddit.com/r/programming/.rss'
+          url = 'http://reddit.com/r/programming/.rss'
           rss = RSS::Parser.parse(open(url).read, false).items.first
           client.say(channel: data.channel, text: "Reddit for programmers, here you go!")
           client.say(channel: data.channel, text: rss.link)
