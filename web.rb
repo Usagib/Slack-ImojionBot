dfc7e5b1721a0f1f9caeaa78744d3082e2c4dbe1
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-01-16 12:13:58 -0600

    herokutesting

--- web.rb
+++ web.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 module SlackImojionbot
   class Web < Sinatra::Base
     get '/' do
-      'Connection succesfull'
+      if params['code']
+        rc = HTTP.post("https://slack.com/api/oauth.access", params: {
+          code: params['code'],
+          client_id: ENV['SLACK_CLIENT_ID'],
+          client_secret: ENV['SLACK_CLIENT_SECRET']
+          })
+      end
     end
   end
 end
