7d98d8f267880df9f2f95145cbf33b5e5c10564b
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-01-16 11:15:37 -0600

    SlackOauthtest

--- web.rb
+++ web.rb
@@ -2,6 +2,10 @@ require 'sinatra/base'
 
 module SlackImojionbot
   class Web < Sinatra::Base
+    before do
+      request.body.rewind
+      puts @request_payload = JSON.parse(request.body.read, symbolize_names: true)
+    end
     get '/' do
       'Connection succesfull'
     end
