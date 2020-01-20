24bf28260f25ba3095397a0bf20f84d44af4aa0c
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-01-20 00:35:10 -0600

    Slack to heroku endpoint config

--- web.rb
+++ web.rb
@@ -3,7 +3,15 @@ require 'sinatra/base'
 module SlackImojionbot
   class Web < Sinatra::Base
     get '/' do
-      'Connection succesfull'
+      # Instantiate a web client
+      client = Slack::Web::Client.new
+      # Request a token using the temporary code
+      rc = client.oauth_access(
+      client_id: ENV['SLACK_CLIENT_ID'],
+      client_secret: ENV['SLACK_CLIENT_SECRET'],
+      code: params[:code]
+    )
+    token = rc['bot']['bot_access_token']
     end
   end
 end
